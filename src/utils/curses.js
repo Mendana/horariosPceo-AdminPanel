2129d58fde36337269fb2bbf2814fb64437790cb
mendana <diegodiazmenda@gmail.com>
2025-06-01 20:51:11 +0200

    Añade las constantes cursesPerGrade y usersPerGrade para la gestión de asignaturas y usuarios por grado

---
+++ src/utils/curses.js
@@ -0,0 +1,50 @@
+export const cursesPerGrade = {
+    'Matematicas': ['Primero', 'Segundo', 'Tercero', 'Cuarto'],
+    'Informatica': ['Primero', 'Segundo', 'Tercero', 'Cuarto'],
+    'Fisica': ['Primero', 'Segundo', 'Tercero', 'Cuarto'],
+    'MatFis': ['Primero', 'Segundo', 'Tercero', 'Cuarto', 'Quinto'],
+    'FisMat': ['Primero', 'Segundo', 'Tercero', 'Cuarto', 'Quinto'],
+    'InfMat': ['Primero', 'Segundo', 'Tercero', 'Cuarto', 'Quinto'],
+}
+
+export const usersPerGrade = {
+    'Matematicas': {
+        'Primero': 'MatPrimero',
+        'Segundo': 'MatSegundo',
+        'Tercero': 'MatTercero',
+        'Cuarto': 'MatCuarto',
+    },
+    'Informatica': {
+        'Primero': 'InfPrimero',
+        'Segundo': 'InfSegundo',
+        'Tercero': 'InfTercero',
+        'Cuarto': 'InfCuarto',
+    },
+    'Fisica': {
+        'Primero': 'FisPrimero',
+        'Segundo': 'FisSegundo',
+        'Tercero': 'FisTercero',
+        'Cuarto': 'FisCuarto',
+    },
+    'MatFis': {
+        'Primero': 'MatFisPrimero',
+        'Segundo': 'MatFisSegundo',
+        'Tercero': 'MatFisTercero',
+        'Cuarto': 'MatFisCuarto',
+        'Quinto': 'MatFisQuinto',
+    },
+    'FisMat': {
+        'Primero': 'FisMatPrimero',
+        'Segundo': 'FisMatSegundo',
+        'Tercero': 'FisMatTercero',
+        'Cuarto': 'FisMatCuarto',
+        'Quinto': 'FisMatQuinto',
+    },
+    'InfMat': {
+        'Primero': 'InfMatPrimero',
+        'Segundo': 'InfMatSegundo',
+        'Tercero': 'InfMatTercero',
+        'Cuarto': 'InfMatCuarto',
+        'Quinto': 'InfMatQuinto',
+    }
+}
