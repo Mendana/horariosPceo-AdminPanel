54a545ce1f3b370f4a14fffda5dd28d92b4e99a5
mendana <diegodiazmenda@gmail.com>
2025-06-05 11:51:55 +0200

    Ajusta el tamaño de fuente en el componente HeaderNav para mejorar la legibilidad en diferentes dispositivos.

--- src/components/HeaderNav.jsx
+++ src/components/HeaderNav.jsx
@@ -2,8 +2,8 @@ export function HeaderNav({ title }) {
     return(
         <section className='flex flex-row justify-between pl-2 sm:px-5 pt-5 pb-2 border-b-2 border-[var(--secondary-black)]'>
             <div>
-                <h1 className="text-3xl font-bold">{title}</h1>
-                <h3 className='text-lg sm:text-xl font-semibold text-[var(--secondary-black)]'>Grados de Informática y Ciencias</h3>
+                <h1 className="text-2xl md:text-3xl font-bold">{title}</h1>
+                <h3 className='text-lg md:text-xl font-semibold text-[var(--secondary-black)]'>Grados de Informática y Ciencias</h3>
             </div>
         </section>
     )
